4b6af3a91ee9d9b777914d4de0f832b1a2c79e9c
biancamusat <biancamusat.98@gmail.com>
2020-08-30 19:39:14 +0300

    All the way up to having 3 weapons

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float healthPoints = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        healthPoints -= damage;
+        if (healthPoints <= 0)
+        {
+            GetComponent<DeathHandler>().HandleDeath();
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 30f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = 0.5f;
+
+    RigidbodyFirstPersonController fpsController;
+
+    bool zoomedInToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!zoomedInToggle)
+            {
+                mainCamera.fieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
+            }
+            else
+            {
+                mainCamera.fieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
+            }
+            zoomedInToggle = !zoomedInToggle;
+        }
+    }
+}
